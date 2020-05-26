e1c8b541c5de2617aa10b26a7040fe4da0679fde
ruben salvago <rubensalvago@hotmail.com>
2020-05-26 16:49:27 +0200

    creacion del metodo 'obtener perimetro'

--- src/main/java/com/mycompany/figuras/Rectangulo.java
+++ src/main/java/com/mycompany/figuras/Rectangulo.java
@@ -10,5 +10,19 @@ package com.mycompany.figuras;
  * @author rubensalvago
  */
 public class Rectangulo {
+    private double lado1;
+    private double lado2;
     
+    public Rectangulo(double l1, double l2){
+        lado1 = l1;
+        lado2 = l2;
+    }
+    
+    public double obteberArea(){
+        return lado1 * lado2;
+    }
+    
+    public double obtenerPerimetro(){
+        return (lado1 * 2) + (lado2 * 2);
+    }
 }
